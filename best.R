c115e60ed665065f8c7a2d09bffd88e3fcbfea70
David Hodgman <github@hodgman.info>
2015-02-22 12:10:32 +1100

    First commit

---
+++ best.R
@@ -0,0 +1,15 @@
+best <- function(state, outcome) {
+	
+	if (!(outcome == "heart attack" | outcome == "heart failure" | outcome == "pneumonia"))
+		stop("invalid outcome")
+	
+	
+	## Read outcome data
+	## Check that state and outcome are valid
+	## Return hospital name in that state with lowest 30-day death
+	## rate
+	
+	## \heart attack", \heart failure", or \pneumonia".
+	
+	print ("ok")
+}
